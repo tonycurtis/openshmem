43a6293dd3e8443d408102698ddc045a0738278e
Tony Curtis <tonyc@uh.edu>
2016-04-04 20:18:35 -0500

    Add FreeBSD kernel-query code to find executable name without requiring /proc (which is not mounted by default)

--- src/utils/exe.c
+++ src/utils/exe.c
@@ -60,13 +60,62 @@
 
 #ifdef __FreeBSD__
 
-static const char *self = "/proc/self/file";
-static const char *self_fmt = "/proc/%ld/file";
+/* getpid() */
+#include <sys/types.h>
+#include <unistd.h>
+
+/* kinfo */
+#include <sys/user.h>
+#include <libutil.h>
+
+/**
+ * inspect our own executable: find the real program name, and open a
+ * file descriptor
+ */
+static
+int
+per_os_executable_init (void)
+{
+    int n;
+    struct kinfo_file *p = kinfo_getfile (getpid (), &n);
+
+    if (p == NULL) {
+      return -1;
+    }
+
+    strncpy (GET_STATE (exe_name), p->kf_path, MAXPATHLEN - 1);
+    return 0;
+}
 
 #elif defined(__linux__)
 
 static const char *self = "/proc/self/exe";
 static const char *self_fmt = "/proc/%ld/exe";
+
+/**
+ * inspect our own executable: find the real program name, and open a
+ * file descriptor
+ */
+static
+int
+per_os_executable_init (void)
+{
+    ssize_t s;
+
+    /* see if the shortcut works */
+    s = readlink (self, GET_STATE (exe_name), MAXPATHLEN - 1);
+
+    /* if not, try finding our PID */
+    if (s >= 0) {
+        /* bleagh, readlink doesn't null-terminate */
+        GET_STATE (exe_name)[s] = '\0';
+    }
+    else {
+        snprintf (GET_STATE (exe_name), MAXPATHLEN, self_fmt, getpid ());
+    }
+
+    return 0;
+}
 
 #else
 
@@ -81,35 +130,32 @@ static const char *self_fmt = "/proc/%ld/exe";
 void
 shmemi_executable_init (void)
 {
-    ssize_t s;
     int fd;
+    int s;
 
-    /* see if the shortcut works */
-    s = readlink (self, GET_STATE (exe_name), MAXPATHLEN - 1);
-
-    /* if not, try finding our PID */
-    if (s >= 0) {
-        /* bleagh, readlink doesn't null-terminate */
-        GET_STATE (exe_name)[s] = '\0';
+    /*
+     * each OS does its magic
+     */
+    s = per_os_executable_init ();
+    if (EXPR_UNLIKELY (s < 0)) {
+        shmemi_trace (SHMEM_LOG_FATAL,
+                      "can't find my own executable name"
+		      );
+        return;
+        /* NOT REACHED */
     }
-    else {
-        snprintf (GET_STATE (exe_name), MAXPATHLEN, self_fmt, getpid ());
-    }
-
+    
     /* get a file descriptor */
     fd = open (GET_STATE (exe_name), O_RDONLY, 0);
     if (EXPR_UNLIKELY (fd < 0)) {
         shmemi_trace (SHMEM_LOG_FATAL,
                       "can't open \"%s\" (%s)",
                       GET_STATE (exe_name), strerror (errno));
-        goto bail;
+        return;
         /* NOT REACHED */
     }
 
     SET_STATE (exe_fd, fd);
-
- bail:
-    return;
 }
 
 /**
